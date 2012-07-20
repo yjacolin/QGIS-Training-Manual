39aeb52874c3b2d75001d0523b143c810e9d2a30
Tim Sutton <tim@linfiniti.com>
2012-07-20 09:14:25 +0200

    Updated pre translate script to actually work

--- pre_translate.sh
+++ pre_translate.sh
@@ -5,11 +5,27 @@ if [ $1 ]; then
   LOCALES=$1
 fi
 
+
+# Create / update the translation catalogue - this will generate the master .pot files
+sphinx-build -b gettext . i18n/pot/
+
+# Now iteratively update the locale specific .po files with any new strings needed translation
 for LOCALE in ${LOCALES}
 do
+  echo "Updating translation catalog for ${LOCAL}:"
+  echo "------------------------------------"
   mkdir -p i18n/${LOCALE}/LC_MESSAGES
-  echo "Updating translation catalog... "
-  # Create / update the translation catalogue
-  sphinx-build -D language=${LOCALE} -b gettext . i18n/${LOCALE}/LC_MESSAGES/
-  echo "Done."
+  for FILE in `ls i18n/pot`
+  do
+    POTFILE=i18n/pot/${FILE}
+    POFILE=i18n/${LOCALE}/LC_MESSAGES/`basename ${POTFILE} .pot`.po
+    if [ -f $POFILE ];
+    then
+      echo "Updating strings for ${POFILE}"
+      msgmerge -U ${POFILE} ${POTFILE}
+    else
+      echo "Creating ${POFILE}"
+      cp ${POTFILE} ${POFILE} 
+    fi
+  done
 done
